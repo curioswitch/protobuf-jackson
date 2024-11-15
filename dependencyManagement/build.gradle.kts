d746e1b7d5745cc160af433ad186f8c118dda5b9
Choko <choko@curioswitch.org>
2024-11-15 10:30:17 +0900

    Update dependencies

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -8,16 +8,16 @@ val dependencyVersions = hashMapOf<String, String>()
 rootProject.extra["versions"] = dependencyVersions
 
 val DEPENDENCY_BOMS = listOf(
-  "com.fasterxml.jackson:jackson-bom:2.18.0",
+  "com.fasterxml.jackson:jackson-bom:2.18.1",
   "com.google.guava:guava-bom:33.3.1-jre",
   "com.google.protobuf:protobuf-bom:${System.getenv("PROTOBUF_VERSION").orEmpty().ifEmpty { "4.28.2" }}",
-  "org.junit:junit-bom:5.11.1",
+  "org.junit:junit-bom:5.11.3",
 )
 
 val DEPENDENCY_SETS = listOf(
   DependencySet(
     "com.google.errorprone",
-    "2.33.0",
+    "2.35.1",
     listOf("error_prone_annotations", "error_prone_core")
   )
 )
@@ -25,8 +25,8 @@ val DEPENDENCY_SETS = listOf(
 val DEPENDENCIES = listOf(
   "com.google.code.findbugs:annotations:3.0.1",
   "com.google.code.findbugs:jsr305:3.0.2",
-  "com.uber.nullaway:nullaway:0.11.3",
-  "net.bytebuddy:byte-buddy:1.15.3",
+  "com.uber.nullaway:nullaway:0.12.1",
+  "net.bytebuddy:byte-buddy:1.15.10",
   "org.assertj:assertj-core:3.26.3",
 )
 
